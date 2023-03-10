8111004a0c7e6c413c7012d08ae1e1da7e15acb3
jayanth-parthsarathy <jparthaa@gmail.com>
2023-03-10 17:52:02 +0530

    Add styling and finalize

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const root = document.getElementById("root");
 let myLibrary = [];
+const warn = document.getElementById("warn");
 const form = document.getElementById("form");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
@@ -29,19 +30,34 @@ function addBookToLibrary() {
 }
 function printBooks(myLibrary) {
   root.innerHTML = "";
-  for (book of myLibrary) {
-    console.log(book);
+  for (let i = 0; i < myLibrary.length; i++) {
     const div = document.createElement("div");
     div.className = "book";
-    const title = document.createTextNode(book.title);
-    div.innerHTML = `
+    div.setAttribute("data-index", `${i}`);
+    if (myLibrary[i].read) {
+      div.innerHTML = `
     <h3>
-      ${book.title}
+      Title: ${myLibrary[i].title}
     </h3>
-    <h5>${book.author}</h5>
-    <p>${book.pages}</p>
-    <p>${book.read}</p>
+    <h5>Author: ${myLibrary[i].author}</h5>
+    <p>No. of pages: ${myLibrary[i].pages}</p>
+    <p>Read</p>
+    <button id="read-btn" onclick=MakeRead(${i})>Already read</button>
+    <button id="delete-btn" onclick=Remove(${i})>Remove</button>
     `;
+    } else {
+      div.innerHTML = `
+    <h3>
+      Title: ${myLibrary[i].title}
+    </h3>
+    <h5>Author: ${myLibrary[i].author}</h5>
+    <p>No. of pages: ${myLibrary[i].pages}</p>
+    <p>Not yet read</p>
+    <button onclick=MakeRead(${i})>Already read</button>
+    <button onclick=Remove(${i})>Remove</button>
+    `;
+    }
+
     // h1.appendChild(title);
     root.appendChild(div);
   }
@@ -57,7 +73,25 @@ submit.onclick = () => {
   let authorVal = author.value;
   let pagesVal = pages.value;
   let readVal = read.checked;
+  if (titleVal == "" || authorVal == "" || pagesVal == "") {
+    warn.innerText = "All fields are required";
+    return;
+  } else {
+    warn.innerText = "";
+  }
   let book = new Book(titleVal, authorVal, pagesVal, readVal);
   myLibrary.push(book);
   printBooks(myLibrary);
 };
+
+function Remove(i) {
+  // let index = div.getAttribute("data-index");
+  // console.log(index);
+  myLibrary.splice(i, 1);
+  printBooks(myLibrary);
+}
+
+function MakeRead(i) {
+  myLibrary[i].read = !myLibrary[i].read;
+  printBooks(myLibrary);
+}
